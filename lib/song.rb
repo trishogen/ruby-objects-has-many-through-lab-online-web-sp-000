943a70759a5fa39f414187c4b28bcf292770d0ae
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 02:08:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,16 @@ class Song
   attr_accessor :artist
   attr_reader :name, :genre
 
+  @@all = []
+  
   def initialize(name, genre)
     @name = name
     @genre = genre
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+  
 end
