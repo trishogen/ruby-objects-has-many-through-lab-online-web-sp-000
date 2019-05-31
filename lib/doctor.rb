48886f787e2a2f16ca5cc45bf832d343c16c543d
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 02:43:25 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -24,9 +24,7 @@ class Doctor
   end
 
   def patients
-    appointments.map do |appointment|
-      appointment.patient
-    end
+    appointments.map {|appointment| appointment.patient}
   end
 
 end
